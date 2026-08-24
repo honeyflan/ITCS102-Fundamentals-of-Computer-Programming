1cbed05bf3da2b163fe740991f45a34400c3ad7e
neleto <honeyletflan@gmail.com>
2026-08-24 10:40:08 +0800

    ACTIVITY3

---
+++ Activity3.py
@@ -0,0 +1,34 @@
+#Using Variables
+
+#int
+age =25
+class_size = 54
+elevations = 1
+Time= 10
+
+#float
+temp =40.0
+speed = 60.9
+weight = 154
+gpa = 4.0
+
+#string
+username = "Neletobits"
+Password = "bubuka1106"
+Date = "2026-08-24"
+
+#Boolean
+is_sun = False
+
+print("My username", username)
+print("My age", age)
+print("My class_size", class_size)
+print("My elevations", elevations)
+print("My Time", Time)
+print("My temp", temp)
+print("My weight", weight)
+print("My gpa", gpa)
+print("My Password", Password)
+print("My Date", Date)
+print("is is Sunny?", is_sun)
+
