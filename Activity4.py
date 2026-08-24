2eb73a81474d6e8dd2922dcb20ebc609c34a9ef7
neleto <honeyletflan@gmail.com>
2026-08-24 17:16:35 +0800

    Activity4 for ITCS102

---
+++ Activity4.py
@@ -0,0 +1,12 @@
+#escape sequence
+# \n - new line
+# \t - tab
+# \r - carriage return/ deprecated/obsolete
+# \b - backspace
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase1 = "\"you can fall in love in an instant, \\its letting go that takes time ,, neleto \nYou go kick the can, \nbut you keep on running"
+phrase2= "Too reach the moon, \nyou have to keep \bon dreaming"
+
+print(phrase1)
