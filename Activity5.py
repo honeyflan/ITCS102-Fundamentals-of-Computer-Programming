07f44296a6130d7a8ade7dd9411af9d9a2ebf382
neleto <honeyletflan@gmail.com>
2026-08-26 13:14:48 +0800

    CODE CHALLENGE AND ACTIVITY 5

---
+++ Activity5.py
@@ -0,0 +1,12 @@
+#Operators Activity
+a = 50
+b = 20
+sum = a + b
+Difference = a - b
+Multiplication = a * b
+Division = a/b
+
+ 
+print("The sum of ",a," and", b," is",sum)
+print("The Difference of ",a," and", b," is" ,Difference)
+print("The product of ",a," and", b," is",a*b)
---
+++ Code_Challenge2.py
@@ -0,0 +1,37 @@
+
+
+a=19863
+T=0
+fiveH=0
+twoH=0
+H=0
+F=0
+ty=0
+ten=0
+one=0
+
+print("My current money is", a)
+T=a//1000 
+a=a%1000
+print("thousand:", T)
+fiveH=a//500
+a=a%500
+print("Fivehundred:", fiveH)
+twoH=a//200
+a=a%200
+print("twohundred:", twoH)
+H=a//100
+a=a%100
+print("Hundred:", H)
+F=a//50
+a=a%50
+print("Fifthy:", F)
+ty=a//20
+a=a%20
+print("Twenty:", ty)
+ten=a//10
+a=a%10
+print("tenth:", ten)
+one=a//1
+a=a%1
+print("ones:", one)
