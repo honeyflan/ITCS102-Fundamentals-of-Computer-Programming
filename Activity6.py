ae2f747570db61ddab3195065dc4585359725d32
neleto <honeyletflan@gmail.com>
2026-09-06 21:28:25 +0800

    Create Activity6.py

---
+++ Activity6.py
@@ -0,0 +1,6 @@
+#dynamic , static
+#dynamic - ever changing
+#static - fixed
+
+name = input("What is your name ? --> Honeylet T. Flancia") #by default input would release a string type data 
+Print("Hello", name)
