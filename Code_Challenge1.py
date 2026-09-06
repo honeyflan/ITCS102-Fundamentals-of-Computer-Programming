de28703bdc6a7fb22c510fffaee091fb2740c8be
neleto <honeyletflan@gmail.com>
2026-09-06 18:18:54 +0800

    Delete Code_Challenge1.py

--- Code_Challenge1.py
+++
@@ -1,7 +0,0 @@
-#CODE CHALLENGE 1
-
-phrase1= "\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\r\t    *\t    *  Hi, My Name is Honeylet T. Flancia   *       *\n\t    *\t    *\t    \t From BSIT-2C  \t\t    *       *\n\t    *\t    *\t    \t\t\t\t    *       *\n\t    *\t    *    *    *    *     *    *    *    *   *\t    *\n\t    *\t    *    *    *    *     *    *    *    *   *\t    *"
-
-
-
-print(phrase1)
