02ab13e269665130732d7cb224f558c21abb2b89
neleto <honeyletflan@gmail.com>
2026-09-06 18:19:47 +0800

    Add files via upload

---
+++ Code_Challenge1.py
@@ -0,0 +1,7 @@
+#CODE CHALLENGE 1
+
+phrase1= "\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\r\t\t\t\t    *\t    *  Hi, My Name is Honeylet T. Flancia   *       *\n\t\t\t\t    *\t    *\t    \t From BSIT-2C  \t\t    *       *\n\t\t\t\t    *\t    *\t    \t\t\t\t    *       *\n\t\t\t\t    *\t    *    *    *    *     *    *    *    *   *\t    *\n\t\t\t\t    *\t    *    *    *    *     *    *    *    *   *\t    *"
+
+
+
+print(phrase1)
