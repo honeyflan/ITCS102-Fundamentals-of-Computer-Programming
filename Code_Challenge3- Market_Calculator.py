4612d2536a4f1b626a3938732184cc8a39c25748
neleto <honeyletflan@gmail.com>
2026-09-11 07:17:35 +0800

    Add files via upload

---
+++ Code_Challenge3- Market_Calculator.py
@@ -0,0 +1,10 @@
+#Global freight calculator
+
+Sender_name = input("Input your name: ")
+print("=======================================")
+print("Welcome, " + Sender_name + ", to the Global Freight Calculator!")
+
+type_of_item = input("What type of item are you shipping today?\n1. Electronics\n2. Furniture\n3. Clothing\n4. Food\n5. Other\nPlease enter the number corresponding to your item type: ")
+print("=======================================")
+
+
